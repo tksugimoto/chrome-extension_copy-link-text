605ae633198e7a465ea427420f1178115d2d0e0f
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-04-09 20:17:31 +0900

    [Update] リンクテキストの重複を除去
    同一リンクテキスト＆URLがページ内に複数あり、
    そのリンクテキストのみが対象である場合
    ユーザーが不必要な選択作業をする必要が無いように重複を除去

--- chrome-extension/content_script.js
+++ chrome-extension/content_script.js
@@ -2,6 +2,11 @@
 	if (window.isAlreadyPrepared) return;
 	window.isAlreadyPrepared = true;
 
+	const uniq = array => {
+		// 重複を除去する
+		return Array.from(new Set(array));
+	};
+
 	const searchLinkTexts = linkUrl => {
 		const links = document.getElementsByTagName("a");
 		return Array.from(links).filter(link => {
@@ -15,9 +20,10 @@
 	chrome.runtime.onMessage.addListener(request => {
 		if (request.method === "searchLinkTexts") {
 			const linkTexts = searchLinkTexts(request.linkUrl);
+			const uniqueLinkTexts = uniq(linkTexts);
 			chrome.runtime.sendMessage({
 				method: "copy",
-				texts: linkTexts
+				texts: uniqueLinkTexts
 			});
 		}
 	});
